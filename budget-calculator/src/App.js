8d6746439a7671b310360e71202ec2e696a2eb55
ssh00n <seunghunshin1284@gmail.com>
2024-01-12 18:17:14 +0900

    App.js & App.css

--- budget-calculator/src/App.js
+++ budget-calculator/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
+  const [expenses, setExpenses] = useState([]);
+
+  const onAddExpense = (newExpense) => {
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, newExpense];
+    });
+    toast.dismiss();
+    toast.success("항목이 추가되었습니다!");
+  };
+
+  const calculateTotalCost = () => {
+    return expenses.reduce(
+      (total, expense) => total + parseFloat(expense.cost),
+      0
+    );
+  };
+
+  const clearList = () => {
+    setExpenses([]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1 className="title">예산 계산기</h1>
+      <div className="content-box">
+        <ExpenseForm onAddExpense={onAddExpense}></ExpenseForm>
+        <ExpenseList
+          expenses={expenses}
+          setExpenses={setExpenses}
+        ></ExpenseList>
+        <Button
+          variant="outlined"
+          onClick={clearList}
+          startIcon={<DeleteIcon />}
+          sx={{ color: "black", margin: "10px 0" }}
         >
-          Learn React
-        </a>
-      </header>
+          목록 비우기
+        </Button>
+      </div>
+      <div className="total-cost">총 지출: {calculateTotalCost()}원</div>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        style={{ top: 0 }}
+      />
     </div>
   );
 }
